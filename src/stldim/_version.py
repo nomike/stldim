a95538678011882ea7cfe9ef6be6ba7e3ad2b8f3
nomike <nomike@nomike.cpm>
2024-05-14 23:03:25 +0200

    Version bump.

--- src/stldim/_version.py
+++ src/stldim/_version.py
@@ -3,6 +3,6 @@ __copyright__ = "Copyright 2024 nomike Postmann"
 __email__ = "nomike@nomike.com"
 __license__ = "LGPL3"
 __title__ = "python-stldim"
-__version__ = "0.0.1dev1"
+__version__ = "0.0.1dev2"
 
 __str__ = f'{__title__} v{__version__} by {__author__} <{__email__}>'
