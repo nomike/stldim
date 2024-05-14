a8a5a99c913dd1cd2f0fc231d5c729d3c383bccb
nomike <nomike@nomike.cpm>
2024-05-14 23:21:34 +0200

    Version bump to 0.0.1a1. Fix application title.

--- src/stldim/_version.py
+++ src/stldim/_version.py
@@ -2,7 +2,7 @@ __author__ = "nomike Postmann"
 __copyright__ = "Copyright 2024 nomike Postmann"
 __email__ = "nomike@nomike.com"
 __license__ = "LGPL3"
-__title__ = "python-stldim"
-__version__ = "0.0.1dev2"
+__title__ = "stldim"
+__version__ = "0.0.1a2"
 
 __str__ = f'{__title__} v{__version__} by {__author__} <{__email__}>'
