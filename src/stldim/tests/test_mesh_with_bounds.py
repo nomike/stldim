a0128054dfd5410b4ef284b1fae6ee1a55e16f17
nomike <nomike@nomike.cpm>
2024-05-28 01:23:45 +0200

    Remove function get_stl_dimensions and change tests to test the MeshWithBounds class directly instead.

--- src/stldim/tests/test_mesh_with_bounds.py
+++ src/stldim/tests/test_mesh_with_bounds.py
@@ -1,10 +1,9 @@
 import stldim
-from stl import mesh
 import pytest
 
 def test_3dbenchy():
-    stl_dimensions = stldim.get_stl_dimensions("tests/3DBenchy.stl")
-
+    stl_dimensions = stldim.MeshWithBounds.from_file("tests/3DBenchy.stl")
+    
     assert pytest.approx(stl_dimensions['minx']) == -29.176
     assert pytest.approx(stl_dimensions['maxx']) == 30.825
     assert pytest.approx(stl_dimensions['miny']) == -15.502
