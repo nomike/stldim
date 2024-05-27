56043eca1c3af02fb2077e45f269b3c8ecfdafa5
nomike <nomike@nomike.cpm>
2024-05-28 01:44:02 +0200

    Code linting.

--- src/stldim/tests/test_get_varname.py
+++ src/stldim/tests/test_get_varname.py
@@ -1,10 +1,19 @@
 import stldim
 
 def test_empty_varname():
+    """
+    Test with an empty varnames
+    """
     assert stldim.get_varname("test.stl", "") == "test_stl"
 
 def test_none_varname():
+    """
+    Test with varname set to None
+    """
     assert stldim.get_varname("test.stl", None) == "test_stl"
 
 def test_varname():
+    """
+    Test with a varname set
+    """
     assert stldim.get_varname("test.stl", "foobar") == "foobar"
